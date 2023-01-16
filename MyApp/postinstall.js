ff3910d342eac63a6ec3b2f70969eecc5ad412a8
Demis Bellot <demis.bellot@gmail.com>
2023-01-16 18:18:25 +0800

    update typings

--- MyApp/postinstall.js
+++ MyApp/postinstall.js
@@ -12,9 +12,17 @@ const files = {
       'servicestack-vue.mjs':    'https://unpkg.com/@servicestack/vue@3/dist/servicestack-vue.mjs',
   },
   typings: {
-      '@servicestack/client/index.d.ts': 'https://unpkg.com/@servicestack/client/dist/index.d.ts',  
-      '@servicestack/vue/index.d.ts':    'https://unpkg.com/@servicestack/vue@3/dist/index.d.ts',
-      '@servicestack/ui/index.d.ts':     'https://unpkg.com/@servicestack/ui/shared.d.ts',
+      'vue/index.d.ts':                    'https://unpkg.com/vue@3/dist/vue.d.ts',
+      '@vue/compiler-core.d.ts':           'https://unpkg.com/@vue/compiler-core@3/dist/compiler-core.d.ts',
+      '@vue/compiler-dom.d.ts':            'https://unpkg.com/@vue/compiler-dom@3/dist/compiler-dom.d.ts',
+      '@vue/runtime-dom.d.ts':             'https://unpkg.com/@vue/runtime-dom@3/dist/runtime-dom.d.ts',
+      '@vue/runtime-core.d.ts':            'https://unpkg.com/@vue/runtime-core@3/dist/runtime-core.d.ts',
+      '@vue/reactivity.d.ts':              'https://unpkg.com/@vue/reactivity@3/dist/reactivity.d.ts',
+      '@vue/shared.d.ts':                  'https://unpkg.com/@vue/shared@3/dist/shared.d.ts',
+      '@servicestack/client/index.d.ts':   'https://unpkg.com/@servicestack/client/dist/index.d.ts',  
+      '@servicestack/vue/index.d.ts':      'https://unpkg.com/@servicestack/vue@3/dist/index.d.ts',
+      '@servicestack/vue/components.d.ts': 'https://unpkg.com/@servicestack/vue@3/dist/components/index.d.ts',
+      '@servicestack/ui/index.d.ts':       'https://unpkg.com/@servicestack/ui/shared.d.ts',
   }
 }
 
