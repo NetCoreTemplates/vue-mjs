a20e1dbfe33fa23649ab42c1d997d1c49af458bd
Demis Bellot <demis.bellot@gmail.com>
2023-01-16 19:13:02 +0800

    remove unused code

--- MyApp/Configure.Ui.cs
+++ MyApp/Configure.Ui.cs
@@ -17,30 +17,13 @@ public class ConfigureUi : IHostingStartup
                 ForbiddenPartial = "~/Pages/Shared/Forbidden.cshtml", //Optional. render error in page instead
             };
             
-            //Referenced in Contacts.cs EvalAllowableEntries
+            //Allow Referencing in #Script expressions, e.g. [Input(EvalAllowableEntries)]
             appHost.ScriptContext.Args[nameof(AppData)] = AppData.Instance;
         });
 }
 
+// Shared App Data
 public class AppData
 {
     internal static readonly AppData Instance = new();
-
-    public Dictionary<string, string> Colors { get; } = new() {
-        {"#ffa4a2", "Red"},
-        {"#b2fab4", "Green"},
-        {"#9be7ff", "Blue"}
-    };
-    public List<string> FilmGenres { get; } = EnumUtils.GetValues<FilmGenre>().Map(x => x.ToDescription());
-
-    public List<KeyValuePair<string, string>> Titles { get; } = EnumUtils.GetValues<Title>()
-        .Where(x => x != Title.Unspecified)
-        .ToKeyValuePairs();
-}
-
-public static class HtmlHelperExtensions
-{
-    public static Dictionary<string, string> ContactColors(this IHtmlHelper html) => AppData.Instance.Colors;
-    public static List<KeyValuePair<string, string>> ContactTitles(this IHtmlHelper html) => AppData.Instance.Titles;
-    public static List<string> ContactGenres(this IHtmlHelper html) => AppData.Instance.FilmGenres;
 }
