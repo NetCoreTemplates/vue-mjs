1814aac52d0b4169b60d0f919dceb9f92d078643
Demis Bellot <demis.bellot@gmail.com>
2023-01-16 12:53:31 +0800

    Change AppTasks to use afterAppHostInit

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -20,43 +20,6 @@ else
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-// app.Use(async (context, next) =>
-// {
-//     // call the remainder of the pipeline
-//     await next.Invoke();
-//
-//     // if it is not 404, ignore the request
-//     // if (context.Response.StatusCode != 404)
-//     //     return;
-//
-//     // the requested path
-//     string requestedPath = context.Request.Path;
-//
-//     // the file name part (may contain slashes at first)
-//     string name = requestedPath;
-//
-//     // if the path contains a slash
-//     int index = requestedPath.LastIndexOf('/');
-//
-//     // trim it to just the name
-//     if (index > -1)
-//         name = requestedPath.Substring(index);
-//
-//     // if the name already has an extension, ignore the request
-//     //TODO: dot may not indicate extension...
-//     if (name.Contains('.'))
-//         return;
-//
-//     // otherwise, if a corresponding .html file exists,
-//     // then process the request with that path.
-//     if (File.Exists(app.Environment.WebRootPath + requestedPath.Replace("/", "\\") + ".html"))
-//     {
-//         context.Response.StatusCode = 200;
-//         context.Request.Path = requestedPath + ".html";
-//         await next.Invoke();
-//     }
-// });
-
 app.UseServiceStack(new AppHost());
 
 app.UseRouting();
@@ -64,7 +27,6 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
-
 
 app.UseStatusCodePagesWithReExecute("/Error", "?status={0}");
 
