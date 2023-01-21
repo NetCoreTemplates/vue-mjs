a801c31f1b417bfa0d0094995878a9aca5ca13f0
Demis Bellot <demis.bellot@gmail.com>
2023-01-21 10:57:56 +0800

    Update postinstall.js

--- MyApp/postinstall.js
+++ MyApp/postinstall.js
@@ -1,28 +1,29 @@
 // Usage: node postinstall.js
 
 const writeTo = './wwwroot/lib'
+const defaultPrefix = 'https://unpkg.com'
 const files = {
   js: {
-      'htmx.js':                 'https://unpkg.com/htmx.org@1/dist/htmx.js',
-      'htmxclasses.js':          'https://unpkg.com/htmx.org@1/dist/ext/class-tools.js',
+      'htmx.js':                 '/htmx.org@1/dist/htmx.js',
+      'htmxclasses.js':          '/htmx.org@1/dist/ext/class-tools.js',
   },
   mjs: {
-      'vue.mjs':                 'https://unpkg.com/vue@3/dist/vue.esm-browser.js',
-      'servicestack-client.mjs': 'https://unpkg.com/@servicestack/client/dist/servicestack-client.mjs',
-      'servicestack-vue.mjs':    'https://unpkg.com/@servicestack/vue@3/dist/servicestack-vue.mjs',
+      'vue.mjs':                 '/vue@3/dist/vue.esm-browser.js',
+      'servicestack-client.mjs': '/@servicestack/client/dist/servicestack-client.mjs',
+      'servicestack-vue.mjs':    '/@servicestack/vue@3/dist/servicestack-vue.mjs',
   },
   typings: {
-      'vue/index.d.ts':                    'https://unpkg.com/vue@3/dist/vue.d.ts',
-      '@vue/compiler-core.d.ts':           'https://unpkg.com/@vue/compiler-core@3/dist/compiler-core.d.ts',
-      '@vue/compiler-dom.d.ts':            'https://unpkg.com/@vue/compiler-dom@3/dist/compiler-dom.d.ts',
-      '@vue/runtime-dom.d.ts':             'https://unpkg.com/@vue/runtime-dom@3/dist/runtime-dom.d.ts',
-      '@vue/runtime-core.d.ts':            'https://unpkg.com/@vue/runtime-core@3/dist/runtime-core.d.ts',
-      '@vue/reactivity.d.ts':              'https://unpkg.com/@vue/reactivity@3/dist/reactivity.d.ts',
-      '@vue/shared.d.ts':                  'https://unpkg.com/@vue/shared@3/dist/shared.d.ts',
-      '@servicestack/client/index.d.ts':   'https://unpkg.com/@servicestack/client/dist/index.d.ts',  
-      '@servicestack/vue/index.d.ts':      'https://unpkg.com/@servicestack/vue@3/dist/index.d.ts',
-      '@servicestack/vue/components.d.ts': 'https://unpkg.com/@servicestack/vue@3/dist/components/index.d.ts',
-      '@servicestack/ui/index.d.ts':       'https://unpkg.com/@servicestack/ui/shared.d.ts',
+      'vue/index.d.ts':                    '/vue@3/dist/vue.d.ts',
+      '@vue/compiler-core.d.ts':           '/@vue/compiler-core@3/dist/compiler-core.d.ts',
+      '@vue/compiler-dom.d.ts':            '/@vue/compiler-dom@3/dist/compiler-dom.d.ts',
+      '@vue/runtime-dom.d.ts':             '/@vue/runtime-dom@3/dist/runtime-dom.d.ts',
+      '@vue/runtime-core.d.ts':            '/@vue/runtime-core@3/dist/runtime-core.d.ts',
+      '@vue/reactivity.d.ts':              '/@vue/reactivity@3/dist/reactivity.d.ts',
+      '@vue/shared.d.ts':                  '/@vue/shared@3/dist/shared.d.ts',
+      '@servicestack/client/index.d.ts':   '/@servicestack/client/dist/index.d.ts',  
+      '@servicestack/vue/index.d.ts':      '/@servicestack/vue@3/dist/index.d.ts',
+      '@servicestack/vue/components.d.ts': '/@servicestack/vue@3/dist/components/index.d.ts',
+      '@servicestack/ui/index.d.ts':       '/@servicestack/ui/shared.d.ts',
   }
 }
 
@@ -34,7 +35,9 @@ const https = require('https')
 Object.keys(files).forEach(dir => {
     const dirFiles = files[dir]
     Object.keys(dirFiles).forEach(name => {
-        const url = dirFiles[name]
+        let url = dirFiles[name]
+        if (url.startsWith('/'))
+            url = defaultPrefix + url
         const toFile = path.join(writeTo, dir, name)
         const toDir = path.dirname(toFile)
         if (!fs.existsSync(toDir)) {
