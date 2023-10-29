55b8d7d73f040526d639b915209ff05c2e5f9a5e
Demis Bellot <demis.bellot@gmail.com>
2023-10-29 21:07:01 +0800

    Update Getting Started Component

--- MyApp/wwwroot/mjs/components/GettingStarted.mjs
+++ MyApp/wwwroot/mjs/components/GettingStarted.mjs
@@ -1,4 +1,4 @@
-import { ref } from "vue"
+import { ref, computed } from "vue"
 import ShellCommand from "./ShellCommand.mjs"
 
 export default {
@@ -12,6 +12,28 @@ export default {
       <input type="text" v-model="project" autocomplete="off" spellcheck="false" @keydown="validateSafeName"
              class="mb-8 sm:text-lg rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 dark:bg-gray-800"/>
 
+        <section class="w-full flex justify-center text-center">
+           <div class="mb-2">
+              <div class="flex justify-center text-center">
+                 <a class="archive-url hover:no-underline netcoretemplates_empty" :href="zipUrl('NetCoreTemplates/vue-mjs')">
+                    <div class="bg-white dark:bg-gray-800 px-4 py-4 mr-4 mb-4 rounded-lg shadow-lg text-center items-center justify-center hover:shadow-2xl dark:border-2 dark:border-pink-600 dark:hover:border-blue-600 dark:border-2 dark:border-pink-600 dark:hover:border-blue-600" style="min-width:150px">
+                       <div class="text-center font-extrabold flex items-center justify-center mb-2">
+                          <div class="text-4xl text-blue-400 my-3">
+                             <svg class="w-14 h-14" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 261.76 226.69">
+                              <g transform="matrix(1.3333 0 0 -1.3333 -76.311 313.34)"><g transform="translate(178.06 235.01)"><path d="m0 0-22.669-39.264-22.669 39.264h-75.491l98.16-170.02 98.16 170.02z" fill="#41b883"/></g><g transform="translate(178.06 235.01)"><path d="m0 0-22.669-39.264-22.669 39.264h-36.227l58.896-102.01 58.896 102.01z" fill="#34495e"/></g></g>
+                            </svg>
+                          </div>
+                       </div>
+                       <div class="text-xl font-medium text-gray-700">Razor Vue</div>
+                       <div class="flex justify-center h-8"></div>
+                       <span class="archive-name px-4 pb-2 text-blue-600 dark:text-indigo-400">{{ projectZip }}</span>
+                       <div class="count mt-1 text-gray-400 text-sm"></div>
+                    </div>
+                 </a>
+              </div>
+           </div>
+        </section>
+
       <ShellCommand class="mb-2">dotnet tool install -g x</ShellCommand>
       <ShellCommand class="mb-2">x new {{template}} {{project}}</ShellCommand>
 
@@ -21,16 +43,25 @@ export default {
       <h4 class="py-6 text-center text-xl">Run .NET Project (New Terminal)</h4>
       <ShellCommand class="mb-2">dotnet watch</ShellCommand>
 
-      <div class="mt-8 text-center">
-        <div class="flex justify-center">
-          <a class="bg-purple-500 text-purple-50 rounded px-2 py-0.5 mx-1" href="/posts/rider">rider</a>
-          <a class="bg-purple-500 text-purple-50 rounded px-2 py-0.5 mx-1" href="/posts/vs">visualstudio</a>
-        </div>
-      </div>        
     </div>`,
     props: { template:String },
     setup(props) {
         const project = ref('ProjectName')
+
+        const projectZip = computed(() => (project.value || 'MyApp') + '.zip')
+
+        /** @param {string} template */
+        const zipUrl = (template) =>
+            `https://account.servicestack.net/archive/${template}?Name=${project.value || 'MyApp'}`
+
+        /** @param {KeyboardEvent} e */
+        const isAlphaNumeric = (e) => {
+            const c = e.charCode;
+            if (c >= 65 && c <= 90 || c >= 97 && c <= 122 || c >= 48 && c <= 57 || c === 95) //A-Za-z0-9_
+                return;
+            e.preventDefault()
+        }
+
         /** @param path {string}
           * @returns {string} */
         const resolvePath = (path) => navigator.userAgent.indexOf("Win") >= 0 ? path.replace(/\//g,'\\') : path
@@ -44,6 +75,6 @@ export default {
                 return false
             }
         }
-        return { project, uiPath, apiPath, validateSafeName }
+        return { project, projectZip, zipUrl, isAlphaNumeric, uiPath, apiPath, validateSafeName }
     }
 }
