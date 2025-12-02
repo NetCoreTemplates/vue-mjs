c3cf02461c5c58fa009a39c4652417ce29ec39d9
Demis Bellot <demis.bellot@gmail.com>
2025-12-02 22:48:51 +0800

    Update Configure.Db.cs

--- MyApp/Configure.Db.cs
+++ MyApp/Configure.Db.cs
@@ -1,16 +1,30 @@
+using Microsoft.EntityFrameworkCore;
 using ServiceStack.Data;
 using ServiceStack.OrmLite;
+using MyApp.Data;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 [assembly: HostingStartup(typeof(MyApp.ConfigureDb))]
 
 namespace MyApp;
 
-// Database can be created with "dotnet run --AppTasks=migrate"   
 public class ConfigureDb : IHostingStartup
 {
     public void Configure(IWebHostBuilder builder) => builder
-        .ConfigureServices((context,services) => services.AddSingleton<IDbConnectionFactory>(
-            new OrmLiteConnectionFactory(
-                context.Configuration.GetConnectionString("DefaultConnection") ?? "App_Data/db.sqlite",
-                SqliteDialect.Provider)));
+        .ConfigureServices((context, services) => {
+            var connectionString = context.Configuration.GetConnectionString("DefaultConnection")
+                                   ?? "DataSource=App_Data/app.db;Cache=Shared";
+            
+            services.AddOrmLite(options => options.UseSqlite(connectionString));
+
+            // $ dotnet ef migrations add CreateIdentitySchema
+            // $ dotnet ef database update
+            services.AddDbContext<ApplicationDbContext>(options => {
+                options.UseSqlite(connectionString, b => b.MigrationsAssembly(nameof(MyApp)));
+                options.ConfigureWarnings(w => w.Ignore(RelationalEventId.PendingModelChangesWarning));
+            });
+            
+            // Enable built-in Database Admin UI at /admin-ui/database
+            services.AddPlugin(new AdminDatabaseFeature());
+        });
 }
